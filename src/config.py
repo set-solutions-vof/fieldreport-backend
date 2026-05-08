23553b38b59aae02c8912ebc042c993752db30d6
Florian <florian.terlinden@group.one>
2026-05-08 11:14:52 +0200

    SET-6 Ensure pydantic settings follows right format

--- src/config.py
+++ src/config.py
@@ -1,11 +1,11 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
+from pydantic import Field
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
-    app_env: str = "development"
-    database_url: str
+    app_env: str = Field(..., env="APP_ENV")
+    database_url: str = Field(..., env="DATABASE_URL")
 
 
 settings = Settings()
